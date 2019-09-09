8031caa1a800a859d0187fc1751a63f62ea873aa
Erica <ericabasak@gmail.com>
2019-09-09 17:34:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-   creater_hash = {:name=>"Grace Hopper"} == {1=>2}
+   creater_hash = {:name=>"Grace Hopper"}
 end
 
 
